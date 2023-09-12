1c3729cf859ca15028df17b0ff2db520e1ead55c
HaileyHryPark <16parkha@gmail.com>
2023-09-12 09:09:04 +0800

    week dots

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,7 +4,8 @@ from .models import Week, Location, Experience
 # Create your views here.
 def index(request):
 	context = {
-	
+		'year_range': range(80),
+        'week_range': range(52),
 	}
 
 	return render(request, 'index.html', context=context)
