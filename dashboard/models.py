b20099905b310a23192b5dc2672cd8c8445ca29d
Harry Park <16parkha@gmail.com>
2023-10-28 16:55:13 +0800

    saving after a long while, changes in models on experience and location instances

--- dashboard/models.py
+++ dashboard/models.py
@@ -54,7 +54,7 @@ class Week(models.Model):
 	# month = models.ForeignKey('Month', on_delete=models.PROTECT, null=True)
 	# year = models.ForeignKey('Year', on_delete=models.PROTECT, null=True)
 
-	location = models.ManyToManyField('LocationInstance', blank=True)
+	location = models.ManyToManyField('LocationInstance', blank=True, null=True)
 
 	experience = models.ManyToManyField('ExperienceInstance', blank=True)
 
@@ -68,6 +68,40 @@ class Week(models.Model):
 		"""Returns the URL to access a detail record for this week."""
 		return reverse('week-detail', args=[str(self.id)])
 
+	def get_location_l_color(self):
+		if self.location.filter(location_type__exact = 'l').count() == 0:
+			""" if the week is not associated with any l location instance """
+			loc = LocationInstance.objects.all().filter(location_type__exact = 'l', start_date__lte=self.start_date, end_date__gte=self.start_date).first()
+			
+			if loc is not None:
+				self.location.add(loc)
+			
+			else:
+				""" if the current l location instance is not yet updated """
+				latest_loc = LocationInstance.objects.all().order_by('-end_date').first()
+				latest_loc.end_date = self.end_date
+				latest_loc.save()
+				self.location.add(loc)
+
+		return self.location.all().filter(location_type__exact = 'l').first().location.base_color
+
+	def get_location_l_color_highlight(self):
+		if self.location.filter(location_type__exact = 'l').count() == 0:
+			""" if the week is not associated with any l location instance """
+			loc = LocationInstance.objects.all().filter(location_type__exact = 'l', start_date__lte=self.start_date, end_date__gte=self.start_date).first()
+			
+			if loc is not None:
+				self.location.add(loc)
+			
+			else:
+				""" if the current l location instance is not yet updated """
+				latest_loc = LocationInstance.objects.all().order_by('-end_date').first()
+				latest_loc.end_date = self.end_date
+				latest_loc.save()
+				self.location.add(loc)
+
+		return self.location.all().filter(location_type__exact = 'l').first().location.highlight_color
+
 	# def update_week(self):
 
     # def display_calendar_name(self):
@@ -78,7 +112,8 @@ class Week(models.Model):
 
 class Location(models.Model):
 	name = models.CharField(max_length=200)
-	color = models.CharField(max_length=7)
+	base_color = models.CharField(max_length=7)
+	highlight_color = models.CharField(max_length=7)
 
 	# weeks = models.ManyToManyField('Week', blank=True)
 
@@ -88,37 +123,38 @@ class Location(models.Model):
 
 class LocationInstance(models.Model):
 	"""docstring for LocationInstane"""
-	location = models.ForeignKey('Location', on_delete=models.PROTECT, null=True)
+	location = models.ForeignKey('Location', on_delete=models.PROTECT, null=True, blank=True)
 
 	start_date = models.DateField()
 	end_date = models.DateField()
+
+	TYPE = (
+        ('l', 'Living'),
+        ('h', 'Hybrid'),
+        ('t', 'Travel'),
+        ('o', 'Other'),
+    )
+
+	location_type = models.CharField(
+        max_length=1,
+        choices=TYPE,
+        default='o',
+    )
 
 	notes = models.TextField(max_length=1000, blank=True)
 
 	def __str__(self):
 		"""String for representing the Model object."""
-		return f'{self.location.name} ({self.location.start_date.strftime("%Y/%m/%d")} - {self.location.end_date.strftime("%Y/%m/%d")})'
+		return f'{self.location.name} ({self.start_date.strftime("%Y/%m/%d")} - {self.end_date.strftime("%Y/%m/%d")})'
 		
 
 
 class Experience(models.Model):
 	name = models.CharField(max_length=200)
-	color = models.CharField(max_length=7)
+	base_color = models.CharField(max_length=7)
+	highlight_color = models.CharField(max_length=7)
 
 	# weeks = models.ManyToManyField('Week', blank=True)
-
-	TYPE = (
-        ('e', 'Education'),
-        ('w', 'Work'),
-        ('o', 'Other'),
-    )
-
-	experience_type = models.CharField(
-        max_length=1,
-        choices=TYPE,
-        blank=True,
-        default='o',
-    )
 
 	def __str__(self):
 		"""String for representing the Model object."""
@@ -132,8 +168,20 @@ class ExperienceInstance(models.Model):
 	start_date = models.DateField()
 	end_date = models.DateField()
 
-	# notes = models.TextField(max_length=1000, blank=True)
+	TYPE = (
+        ('e', 'Education'),
+        ('w', 'Work'),
+        ('o', 'Other'),
+    )
+
+	experience_type = models.CharField(
+        max_length=1,
+        choices=TYPE,
+        default='o',
+    )
+
+	notes = models.TextField(max_length=1000, blank=True)
 
 	def __str__(self):
 		"""String for representing the Model object."""
-		return f'{self.experience.name} ({self.experience.start_date.strftime("%Y/%m/%d")} - {self.experience.end_date.strftime("%Y/%m/%d")})'
+		return f'{self.experience.name} ({self.start_date.strftime("%Y/%m/%d")} - {self.end_date.strftime("%Y/%m/%d")})'
